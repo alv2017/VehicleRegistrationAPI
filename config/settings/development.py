5c1487ec2cf33448480e183134d0ccab3b996af2
alv2017 <v.alishauskaite@gmail.com>
2021-07-08 19:40:44 +0300

    Redundant print string has been removed.

--- config/settings/development.py
+++ config/settings/development.py
@@ -4,7 +4,6 @@ from .base import *
 DEBUG = True
 
 DATABASES['default'] = DATABASES['development']
-print(DATABASES['default'])
 
 if 'test' in sys.argv or 'pytest' in sys.argv:
     DATABASES['default'] = DATABASES['test']
